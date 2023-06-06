748ea10fc3da5c86982edd20b7068ae8851efe92
avyprem <130694108+avyprem@users.noreply.github.com>
2023-06-06 13:55:55 -0400

    Update AppendInfo.py

--- AppendInfo.py
+++ AppendInfo.py
@@ -1,1 +1,16 @@
 # tkinter tutorial https://www.pythontutorial.net/tkinter/tkinter-hello-world/
+import csv
+import tkinter as tk
+
+root = tk.Tk()
+root.title('moneymanager')
+root.resizable(100,100)
+root.geometry(600,300,300)
+
+root.mainloop()
+
+def saveaccountdata():
+   with open("account.txt", "a") as file:
+        file.write(f"{}|{}|{}|{}|{}\n")
+        print("Data saved to file")
+
