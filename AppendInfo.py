6f0c53db5c3068a1baf75c6e1ff60c06ea57b8a6
avyprem <130694108+avyprem@users.noreply.github.com>
2023-06-06 18:34:02 -0400

    Update AppendInfo.py

--- AppendInfo.py
+++ AppendInfo.py
@@ -11,6 +11,9 @@ root.mainloop()
 
 def saveaccountdata():
    with open("account.txt", "a") as file:
-        file.write(f"{}|{}|{}|{}|{}\n")
-        print("Data saved to file")
+        file.write(f"First Name: {first_name}\n")
+        file.write(f"Last Name: {last_name}\n")
+        file.write(f"Age: {age}\n")
+        file.write(f"Monthly Income: {income}\n")
+        file.write("-----------------------------\n")
 
